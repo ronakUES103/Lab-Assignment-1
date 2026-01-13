b41b16b797692b67b7567d28c73aeb0faf0f2ed9
ronakUES103 <kanishkamittal882@gmail.com>
2026-01-13 09:03:34 +0530

    Add files via upload

---
+++ main-5.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("numerator: ");
+    scanf("%d", &a);
+
+    printf("denominator: ");
+    scanf("%d", &b);
+
+    printf("The fraction is %d/%d\n", a, b);
+
+    return 0;
+}
